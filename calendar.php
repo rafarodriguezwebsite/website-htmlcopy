0f400870ba3c59108fae5f28007f1564dcf44c1a
rafarodriguezwebsite <48632691+rafarodriguezwebsite@users.noreply.github.com>
2019-04-05 11:11:55 -0700

    Update calendar.php

--- calendar.php
+++ calendar.php
@@ -88,7 +88,9 @@ if (empty($events)) {
             $day = intval(substr($event->start->dateTime,8,2));
             $month = $_MONTH[ intval(substr($event->start->dateTime,5,2)) - 1];
             $year = substr($event->start->dateTime,0,4);
-            $time = ( (12+intval(substr($event->start->dateTime,11,2))) % 12 ) . substr($event->start->dateTime,13,3) . ( intval(substr($event->start->dateTime,11,2)) <= 12 ? ' AM':' PM' );
+            $time = ( ( intval(substr($event->start->dateTime,11,2)) % 12 ) == 0 ) ? 12 : ( intval(substr($event->start->dateTime,11,2)) % 12 ) . 
+                substr($event->start->dateTime,13,3) .
+                ( intval(substr($event->start->dateTime,11,2)) <= 12 ? ' AM':' PM' );
         }
         echo "
 <table border='0' class='Event'>
